c63d8be1e376de193f895894b29552f70e53a92e
andbardii <andbardii@icloud.com>
2024-03-18 16:43:25 +0100

    add

--- basics-console/bin/basics_console.dart
+++ basics-console/bin/basics_console.dart
@@ -1,4 +1,3 @@
-import 'package:basics_console/basics_console.dart' as basics_console;
 
 void main(List<String> arguments) {
   //print('Hello world: ${basics_console.calculate()}!');
@@ -7,22 +6,22 @@ void main(List<String> arguments) {
 
   num numero = 0.5;
   int intero = 0;
-  int? vuoto = null;
+  int? vuoto;
   double decimale = 0.00;
   String stringa = "Hello world";
   bool booleano = true;
   List lista = ["Hello", "world"];
   Set set = {5, 6, 7};
   Map mappa = {'nome': 'Federico', 'cognome': 'Serti'};
-  Runes runa = new Runes('\u{1f605}');
+  Runes runa = Runes('\u{1f605}');
 
   //! VARIABILI E COSTANTI
 
-  var variabile;
+  String variabile;
   variabile = 'Hello world';
   //print(variabile);
 
-  final finale;
+  final String finale;
   finale = 'Hello world';
   //print(finale);
 
